24272b5fdb113abe631ae10b8a651068f6011b97
Toshiki Yamaguchi <yama.d.ironman.8104@gmail.com>
2026-06-20 09:40:11 +0900

    Refactor ft_popen for better error handling
    Refactor ft_popen function to improve error handling and code structure.

--- level01/popen/popen.c
+++ level01/popen/popen.c
@@ -3,20 +3,44 @@
 
 int	ft_popen(const char *file, char *const argv[], char type)
 {
-	int	fd[2], pid;
+	int	fd[2];
+	int	pid;
 
-	if (!file || !argv || (type != 'r' && type != 'w') || pipe(fd) || (pid = fork()) < 0)
+	if (!file || !argv || (type != 'r' && type != 'w'))
 		return (-1);
-	if (!pid)
+	if (pipe(fd) < 0)
+		return (-1);
+	pid = fork();
+	if (pid < 0)
 	{
-		if ((type == 'r' && dup2(fd[1], 1) < 0) || (type == 'w' && dup2(fd[0], 0) < 0))
-			exit(1);
 		close(fd[0]);
 		close(fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		if (type == 'r')
+		{
+			close(fd[0]);
+			if (dup2(fd[1], 1) < 0)
+				exit(1);
+			close(fd[1]);
+		}
+		else
+		{
+			close(fd[1]);
+			if (dup2(fd[0], 0) < 0)
+				exit(1);
+			close(fd[0]);
+		}
 		execvp(file, argv);
 		exit(1);
 	}
 	if (type == 'r')
-		return (close(fd[1]), fd[0]);
-	return (close(fd[0]), fd[1]);
+	{
+		close(fd[1]);
+		return (fd[0]);
+	}
+	close(fd[0]);
+	return (fd[1]);
 }
